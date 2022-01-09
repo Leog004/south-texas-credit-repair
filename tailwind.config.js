e72def1d9738ef4bdfc6e8097ba9aba4b430e530
LEO\leog4 <leog4za@gmail.com>
2022-01-09 15:40:59 -0600

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        height: {
+          sm: '10rem',
+    
+          md: '20rem',
+    
+          lg: '30rem',
+    
+          xl: '40rem',
+
+          xxl: '50rem',
+        },
+    },
+  },
+  plugins: [],
+}
