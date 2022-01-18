5f9d365aafa3632efb2be36b575fc2388efa82c4
LEO\leog4 <leog4za@gmail.com>
2022-01-17 21:28:54 -0600

    adding email

--- Email/index.js
+++ Email/index.js
@@ -5,7 +5,7 @@ export default async function SendEmailContact_Message_Dealer(body) {
     let request = await axios
       .post("/api/email", {
         name: "South Texas Credit Repair",
-        email: "leog4za@gmail.com",
+        email: "southtexascreditrepair2019@gmail.com",
         subject: `New Appointment`,
         body: body
       })
