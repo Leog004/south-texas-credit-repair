0bd4f8246dabf86f9d3b50bea8e8d67032833212
LEO\leog4 <leog4za@gmail.com>
2022-01-29 16:46:24 -0600

    adding sendgrid

--- pages/api/email.js
+++ pages/api/email.js
@@ -17,74 +17,74 @@ mail.setApiKey(process.env.SENDGRID)
 */
 
 
-export default async function emailApi(req, res) {
+// export default async function emailApi(req, res) {
 
-    const {email, name, subject} = req.body;
-    const {Username, Useremail, Userphone, Userdate, UserAppointment} = req.body.body || 'Empty user'
-
-    var sendSmtpEmail = {
-        to: [{
-            email,
-            email,
-            subject
-        }],
-        templateId: 1,
-        params: {
-            date: Userdate,
-            name: Username,
-            email: Useremail,
-            phone: Userphone,
-            appointment: UserAppointment
-        },
-        headers: {
-            'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
-        }
-    };
-
-    try{
-        sendInBlue(sendSmtpEmail);
-    }catch(err){
-        console.log(err);
-    }
-    
-
-    console.log('Passed Send In Blue');
-
-    await res.send(req.body)
-}
-
-
-// export default async function sendGridEmailAPI(req, res){
-
-    
 //     const {email, name, subject} = req.body;
 //     const {Username, Useremail, Userphone, Userdate, UserAppointment} = req.body.body || 'Empty user'
-    
-//   try{
-//         const message = `
-//             Name: ${Username}\r\n
-//             Email: ${Useremail}\r\n
-//             Phone: ${Userphone}\r\n
-//             Date: ${Userdate}\r\n
-//             Appointment: ${UserAppointment}\r\n
-//         `;
 
-//         const data = {
-//             to: email,
-//             from: email,
-//             subject: subject,
-//             text: message,
-//             html: message.replace(/\r\n/g, '<br>')
-//         };
-        
-//        const result = await mail.send(data);
+//     var sendSmtpEmail = {
+//         to: [{
+//             email,
+//             email,
+//             subject
+//         }],
+//         templateId: 1,
+//         params: {
+//             date: Userdate,
+//             name: Username,
+//             email: Useremail,
+//             phone: Userphone,
+//             appointment: UserAppointment
+//         },
+//         headers: {
+//             'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
+//         }
+//     };
 
-//        if(result){ 
-//            console.log(result);
-//            return res.status(200).send(result);
-//        }
-
-//     }catch(error){
-//         return res.status(400).send(error);
+//     try{
+//         sendInBlue(sendSmtpEmail);
+//     }catch(err){
+//         console.log(err);
 //     }
+    
+
+//     console.log('Passed Send In Blue');
+
+//     await res.send(req.body)
 // }
+
+
+export default async function sendGridEmailAPI(req, res){
+
+    
+    const {email, name, subject} = req.body;
+    const {Username, Useremail, Userphone, Userdate, UserAppointment} = req.body.body || 'Empty user'
+    
+  try{
+        const message = `
+            Name: ${Username}\r\n
+            Email: ${Useremail}\r\n
+            Phone: ${Userphone}\r\n
+            Date: ${Userdate}\r\n
+            Appointment: ${UserAppointment}\r\n
+        `;
+
+        const data = {
+            to: 'leog4za@gmail.com',
+            from: 'lgnetworkdesign4@gmail.com',
+            subject: subject,
+            text: message,
+            html: message.replace(/\r\n/g, '<br>')
+        };
+        
+       const result = await mail.send(data);
+
+       if(result){ 
+           console.log(result);
+           return res.status(200).send(result);
+       }
+
+    }catch(error){
+        return res.status(400).send(error);
+    }
+}
