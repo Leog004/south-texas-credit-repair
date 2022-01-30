08ec7593470d5ac1e86107d721322c682ad1d229
LEO\leog4 <leog4za@gmail.com>
2022-01-29 19:20:26 -0600

    adding contact functionality

--- pages/Location/index.js
+++ pages/Location/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Hero } from '../../components'
 import { getLocationPageData } from '../../services';
 import { toast } from 'react-toastify';
@@ -97,8 +97,8 @@ const onInputChange = (e) => {
                       <p className="text-base">{data.profileDescription}</p>
                     </div>
                   </div>
-                  <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                    <p class="leading-relaxed text-lg mb-4">{data.sectionDescription}</p>
+                  <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                    <p className="leading-relaxed text-lg mb-4">{data.sectionDescription}</p>
                   </div>
                 </div>
               </div>
